33b352af17714c255ec2714b78663956969b6499
Yusuf <y.abdulla37@gmail.com>
2022-07-26 20:00:49 -0600

    removed unused images and code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,41 +38,6 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* <Navbar
-        sticky="top"
-        variant="dark"
-        style={{ backgroundColor: 'black', paddingBottom: '1em', marginBottom: '2em' }}
-      >
-        <Container>
-          <Navbar.Brand href="#home" className="h1" style={{ color: 'white', fontSize: '2em' }}>
-            Yusuf Abdulla
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#hero" style={{ color: 'white', fontSize: '2em' }}>
-              Home
-            </Nav.Link>
-            <Nav.Link href="#about" style={{ color: 'white', fontSize: '2em' }}>
-              About
-            </Nav.Link>
-            <Nav.Link href="#experience" style={{ color: 'white', fontSize: '2em' }}>
-              Experience
-            </Nav.Link>
-            <Nav.Link href="#projects" style={{ color: 'white', fontSize: '2em' }}>
-              Projects
-            </Nav.Link>
-            <Nav.Link href="#contact" style={{ color: 'white', fontSize: '2em' }}>
-              Contact
-            </Nav.Link>
-            <Nav.Link
-              href="https://resume.creddle.io/resume/hotd8oqyo94"
-              target="_blank"
-              style={{ color: 'white', fontSize: '2em' }}
-            >
-              Resume
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar> */}
       <PortfolioProvider value={{ hero, about, experience, projects, contact, footer }}>
         <Hero />
         <About />
